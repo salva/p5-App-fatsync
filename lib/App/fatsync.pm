e1678cdc2988f3cae304e545e2e0c9521e630d8d
Salvador Fandino <sfandino@yahoo.com>
2011-09-23 15:51:40 +0200

    working on it

--- lib/App/fatsync.pm
+++ lib/App/fatsync.pm
@@ -1,34 +1,157 @@
 package App::fatsync;
 
-use 5.012004;
-use strict;
-use warnings;
-
-require Exporter;
-
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use App::fatsync ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
 our $VERSION = '0.01';
 
+use 5.010;
 
-# Preloaded methods go here.
+use strict;
+use warnings;
+use Carp;
+use File::Path qw(make_path);
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT = qw(fatsync);
+
+
+
+
+sub new {
+    my ($class, %opts) = @_;
+    my $self = { debug => 0,
+                 opts => \%opts };
+    bless $self, $class;
+}
+
+sub _debug {
+    return unless shift->{debug};
+    print STDERR join(': ', debug => @_), "\n";
+}
+
+sub fatsync {
+    my ($self, $src, $dst) = @_;
+
+    -d $src or croak "$src is not a directory";
+
+    $dst = File::Spec->rel2abs($dst);
+
+    make_path($dst);
+    -d $dst or croak "could not create $dst dir";
+
+    $self->_fatsync($src, $dst);
+}
+
+sub _error {
+    shift;
+    print STDERR join(': ', @_), "\n";
+}
+
+sub _fatorize {
+    my ($self, $name) = @_;
+    $name =~ s/[:?"]/_/g;
+    $name;
+}
+
+sub _fatsync {
+    my ($self, $src0, $dst0) = @_;
+
+    local $self->{fatorized} = {};
+
+    my @rel = ('.');
+    my %mode;
+
+    while (@rel) {
+        my $rel = shift @rel;
+        my $rel_fat = $self->_fatorize($rel);
+        next unless defined $rel_fat;
+        my $src = File::Spec->rel2abs($rel, $src0);
+        my $dh;
+        unless (opendir $dh, $src) {
+            $self->_error("unable to open directory $src", $!);
+            next;
+        }
+        my $dst = File::Spec->rel2abs($rel_fat, $dst0);
+
+        $self->_debug("mkdir $dst");
+        mkdir $dst, 0700;
+        unless (-d $dst) {
+            $self->_error("unable to create directory $dst");
+        }
+        $mode{$dst} = (stat $src)[2];
+
+        while (defined(my $entry = readdir $dh)) {
+            next if $entry =~ /^\.\.?$/;
+            my $rel_entry = File::Spec->join($rel, $entry);
+            my $rel_entry_fat = $self->_fatorize($rel_entry);
+            next unless defined $rel_entry_fat;
+            my $abs_entry = File::Spec->rel2abs($rel_entry, $src0);
+            my $abs_entry_fat = File::Spec->rel2abs($rel_entry_fat, $dst0);
+            $self->_debug("rel: $rel, entry: $entry, rel_entry: $rel_entry, rel_entry_fat: $rel_entry_fat, abs_entry: $abs_entry, abs_entry_fat: $abs_entry_fat");
+
+            if (-d $abs_entry) {
+                push @rel, $rel_entry;
+            }
+            elsif (-f $abs_entry) {
+                $self->_cp($abs_entry, $abs_entry_fat);
+            }
+        }
+        unless (closedir $dh) {
+            $self->_error("unable to read directory $src", $!);
+            next;
+        }
+    }
+}
+
+sub _cp {
+    my ($self, $src, $dst) = @_;
+    my ($from, $to);
+
+    $self->_debug("copying $src to $dst");
+
+    unless (open $from, '<', $src) {
+        $self->_error("Unable to open $src for reading", $!);
+        return;
+    }
+
+    unless (open $to, '>', $dst) {
+        $self->_error("Unable top open $dst for writing", $!);
+        return;
+    }
+
+    return;
+
+    binmode $from;
+    binmode $to;
+    my $eof;
+
+    while (1) {
+        my $buffer = '';
+        if (!eof and length $buffer < 128 * 1024) {
+            my $bytes = sysread $from, $buffer, 16 * 1024, length $buffer;
+            $eof = 1 unless $bytes;
+        }
+        if (length $buffer) {
+            my $bytes = syswrite $to, $buffer, 16 * 1024;
+            if ($bytes) {
+                substr($buffer, 0, $bytes, '');
+            }
+            else {
+                goto ERROR;
+            }
+        }
+        elsif ($eof) {
+            last;
+        }
+    }
+    close $to or goto ERROR;
+    return 1;
+
+ ERROR:
+    my $err = $!;
+    unlink $to;
+    $self->_error("write to $dst failed", $err);
+    0;
+}
 
 1;
 __END__
